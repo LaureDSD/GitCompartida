7f6e1f1b83f8774c6ea4fe3059b339fb884b8b57
Laureano <laureanodesousa12@gmail.com>
2025-02-18 09:13:28 +0100

    Second commit

---
+++ Proyecto/src/main/java/ProyectoApiLaureano/ProyectoApiLaureano/ProyectoApiLaureanoApplication.java
@@ -0,0 +1,13 @@
+package ProyectoApiLaureano.ProyectoApiLaureano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiLaureanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiLaureanoApplication.class, args);
+	}
+
+}
